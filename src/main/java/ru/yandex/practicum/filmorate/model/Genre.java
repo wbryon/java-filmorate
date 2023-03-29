f2542c1b320efe038e4cce926a67a9a8c2c115d1
wbryon <tamirbudaev.habibula@gmail.com>
2023-03-29 22:51:57 +0300

    Добавлены классы Genre и MpaRating в пакет model

---
+++ src/main/java/ru/yandex/practicum/filmorate/model/Genre.java
@@ -0,0 +1,6 @@
+package ru.yandex.practicum.filmorate.model;
+
+public class Genre {
+    private int genreId;
+    private String name;
+}
---
+++ src/main/java/ru/yandex/practicum/filmorate/model/MpaRating.java
@@ -0,0 +1,6 @@
+package ru.yandex.practicum.filmorate.model;
+
+public class MpaRating {
+    private int ratingId;
+    private String name;
+}
