fa9b445aed053f9e63d752518567ca135475441e
wbryon <tamirbudaev.habibula@gmail.com>
2023-04-01 13:58:11 +0300

    Добавлены интерфейсы GenreStorage и MpaRatingStorage

---
+++ src/main/java/ru/yandex/practicum/filmorate/storage/GenreStorage.java
@@ -0,0 +1,12 @@
+package ru.yandex.practicum.filmorate.storage;
+
+import ru.yandex.practicum.filmorate.model.Genre;
+
+import java.util.Collection;
+import java.util.Optional;
+
+public interface GenreStorage {
+    Collection<Genre> getAll();
+
+    Optional<Genre> getById(int id);
+}
